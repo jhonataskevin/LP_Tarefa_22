a9387ba27f6baac0f544f1301a8ccfd05be9bd0e
Jhonatas Kévin <jhonataskevin552@gmail.com>
2024-10-14 14:28:52 -0300

    Create bee2424.c

---
+++ bee2424.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int x,y;
+   
+   scanf("%d", &x);
+   scanf("%d", &y);
+   
+   if(x >= 0 && x <= 432 && y >= 0 && y <= 468){
+   		printf("dentro\n");
+   }else{
+   	
+   		printf("fora\n");
+   }
+   
+ 
+    return 0;
+}
